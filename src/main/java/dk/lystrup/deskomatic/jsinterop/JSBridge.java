7e024f9f112bc0558742ecdebae6ac556266b051
rolfba <rolf@fambagge.dk>
2016-04-02 20:35:04 +0200

    Updated Runtime Info to use unit/value/fixed json just as sigar info's

--- src/main/java/dk/lystrup/deskomatic/jsinterop/JSBridge.java
+++ src/main/java/dk/lystrup/deskomatic/jsinterop/JSBridge.java
@@ -21,15 +21,39 @@ public class JSBridge {
 
         Runtime runtime = Runtime.getRuntime();
 
-        json.addProperty("numProcessors", runtime.availableProcessors());
-        json.addProperty("freeMemory", runtime.freeMemory());
-        json.addProperty("maxMemory", runtime.maxMemory());
-        json.addProperty("totalMemory", runtime.totalMemory());
+        double freeMemory = runtime.freeMemory() / 1024.0 / 1024.0;
+        double totalMemory = runtime.totalMemory() / 1024.0 / 1024.0;
+        
+        json.add("numProcessors", JSBridge.getJsonWithUnits(runtime.availableProcessors(), null, "# CPU's", 0));
+        json.add("freeMemory", JSBridge.getJsonWithUnits(freeMemory, "MB", "Free memory", 1));
+        json.add("maxMemory", JSBridge.getJsonWithUnits(runtime.maxMemory() / 1024.0 / 1024.0, "MB", "Max memory", 1));
+        json.add("totalMemory", JSBridge.getJsonWithUnits(totalMemory, "MB", "Total memory", 1));
+        json.add("usedMemory", JSBridge.getJsonWithUnits(totalMemory - freeMemory, "MB", "Used memory", 1));
 
         return json.toString();
     }
-    
+
     public SigarInfo getSigar() {
         return SigarInfo.instance();
+    }
+
+    public static JsonObject getJsonWithUnits(Object value, String unit, String description, int fixedDecimals) {
+        JsonObject json = new JsonObject();
+
+        if (value instanceof Number) {
+            json.addProperty("value", (Number) value);
+        } else if (value instanceof Boolean) {
+            json.addProperty("value", (Boolean) value);
+        } else if (value instanceof String) {
+            json.addProperty("value", (String) value);
+        } else if (value instanceof Character) {
+            json.addProperty("value", (Character) value);
+        }
+
+        json.addProperty("unit", unit);
+        json.addProperty("description", description);
+        json.addProperty("fixedDecimals", fixedDecimals);
+
+        return json;
     }
 }
--- src/main/java/dk/lystrup/deskomatic/jsinterop/SigarInfo.java
+++ src/main/java/dk/lystrup/deskomatic/jsinterop/SigarInfo.java
@@ -24,18 +24,19 @@ import org.hyperic.sigar.SigarException;
  * @author Gof
  */
 public class SigarInfo {
+
     private static SigarInfo instance;
-    
+
     public static SigarInfo instance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new SigarInfo();
         }
-        
+
         return instance;
     }
-    
+
     private Sigar sigar;
-    
+
     private SigarInfo() {
         final File location = new File("./sigar-native");
         try {
@@ -43,106 +44,84 @@ public class SigarInfo {
         } catch (Exception ex) {
             Logger.getLogger(SigarInfo.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         sigar = new Sigar();
     }
-    
+
     public String getCpuInfo() {
         JsonObject json = new JsonObject();
-        
+
         try {
             CpuPerc cpu = sigar.getCpuPerc();
-            
-            json.add("cpuIdle", getJsonWithUnits(cpu.getIdle(), null, "Cpu idle", 2));
-            json.add("cpuUser", getJsonWithUnits(cpu.getUser(), null, "Cpu user", 2));
-            json.add("cpuSystem", getJsonWithUnits(cpu.getSys(), null, "Cpu system", 2));
-            json.add("cpuWait", getJsonWithUnits(cpu.getWait(), null, "Cpu wait", 2));
-            json.add("cpuIrq", getJsonWithUnits(cpu.getIrq(), null, "Cpu irq", 2));
-            json.add("cpuNice", getJsonWithUnits(cpu.getNice(), null, "Cpu nice", 2));
-            json.add("cpuCombined", getJsonWithUnits(cpu.getCombined(), null, "Cpu combined", 2));
-            
+
+            json.add("cpuIdle", JSBridge.getJsonWithUnits(cpu.getIdle(), null, "Cpu idle", 2));
+            json.add("cpuUser", JSBridge.getJsonWithUnits(cpu.getUser(), null, "Cpu user", 2));
+            json.add("cpuSystem", JSBridge.getJsonWithUnits(cpu.getSys(), null, "Cpu system", 2));
+            json.add("cpuWait", JSBridge.getJsonWithUnits(cpu.getWait(), null, "Cpu wait", 2));
+            json.add("cpuIrq", JSBridge.getJsonWithUnits(cpu.getIrq(), null, "Cpu irq", 2));
+            json.add("cpuNice", JSBridge.getJsonWithUnits(cpu.getNice(), null, "Cpu nice", 2));
+            json.add("cpuCombined", JSBridge.getJsonWithUnits(cpu.getCombined(), null, "Cpu combined", 2));
+
         } catch (SigarException ex) {
             Logger.getLogger(SigarInfo.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return json.toString();
     }
 
     private long lastTimestamp = -1;
     private Map<String, Long> lastReadBytesMap = new HashMap<>();
     private Map<String, Long> lastWriteBytesMap = new HashMap<>();
-    
+
     public String getDiskInfo() {
         JsonObject json = new JsonObject();
-        
+
         try {
             FileSystem[] fsList = sigar.getFileSystemList();
-            
-            for(FileSystem fs : fsList) {
+
+            for (FileSystem fs : fsList) {
                 JsonObject fsJson = new JsonObject();
-                
+
                 FileSystemUsage fsInfo = sigar.getFileSystemUsage(fs.getDirName());
-                
-                fsJson.add("usePercent", getJsonWithUnits(fsInfo.getUsePercent() * 100.0, "%", "Percent used", 1));
-                fsJson.add("free", getJsonWithUnits(fsInfo.getFree() / 1024.0 / 1024, "GB", "Free space", 1));
-                fsJson.add("used", getJsonWithUnits(fsInfo.getUsed()/ 1024.0 / 1024, "GB", "Used space", 1));
-                fsJson.add("total", getJsonWithUnits(fsInfo.getTotal()/ 1024.0 / 1024, "GB", "Total space", 1));
-                
+
+                fsJson.add("usePercent", JSBridge.getJsonWithUnits(fsInfo.getUsePercent() * 100.0, "%", "Percent used", 1));
+                fsJson.add("free", JSBridge.getJsonWithUnits(fsInfo.getFree() / 1024.0 / 1024, "GB", "Free space", 1));
+                fsJson.add("used", JSBridge.getJsonWithUnits(fsInfo.getUsed() / 1024.0 / 1024, "GB", "Used space", 1));
+                fsJson.add("total", JSBridge.getJsonWithUnits(fsInfo.getTotal() / 1024.0 / 1024, "GB", "Total space", 1));
+
                 long writeBytes = fsInfo.getDiskWriteBytes();
                 long readBytes = fsInfo.getDiskReadBytes();
 
-                if(lastTimestamp != -1) {
-                
+                if (lastTimestamp != -1) {
+
                     long lastReadBytes = lastReadBytesMap.get(fs.getDirName());
                     long lastWriteBytes = lastWriteBytesMap.get(fs.getDirName());
-                
+
                     double time = (System.currentTimeMillis() - lastTimestamp) / 1000.0;
                     double writeSpeed = (writeBytes - lastWriteBytes) / time;
                     double readSpeed = (readBytes - lastReadBytes) / time;
-                    
-                    fsJson.add("writeSpeed", getJsonWithUnits(writeSpeed / 1024, "KB/s", "Write speed", 1));
-                    fsJson.add("readSpeed", getJsonWithUnits(readSpeed / 1024, "KB/s", "Read speed", 1));
+
+                    fsJson.add("writeSpeed", JSBridge.getJsonWithUnits(writeSpeed / 1024, "KB/s", "Write speed", 1));
+                    fsJson.add("readSpeed", JSBridge.getJsonWithUnits(readSpeed / 1024, "KB/s", "Read speed", 1));
                 }
-                
+
                 lastReadBytesMap.put(fs.getDirName(), readBytes);
                 lastWriteBytesMap.put(fs.getDirName(), writeBytes);
-                
+
                 json.add(fs.getDevName(), fsJson);
             }
         } catch (SigarException ex) {
             Logger.getLogger(SigarInfo.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         lastTimestamp = System.currentTimeMillis();
-        
+
         return json.toString();
     }
-    
+
     public String getNetworkInfo() {
         JsonObject json = new JsonObject();
 
-
-        
         return json.toString();
-    }
-    
-    public JsonObject getJsonWithUnits(Object value, String unit, String description, int fixedDecimals) {
-        JsonObject json = new JsonObject();
-        
-        if(value instanceof Number) {
-            json.addProperty("value", (Number) value);
-        } else if(value instanceof Boolean) {
-            json.addProperty("value", (Boolean) value);
-        } else if(value instanceof String) {
-            json.addProperty("value", (String) value);
-        } else if(value instanceof Character) {
-            json.addProperty("value", (Character) value);
-        }
-        
-        json.addProperty("unit", unit);
-        json.addProperty("description", description);
-        json.addProperty("fixedDecimals", fixedDecimals);
-        
-        return json;
     }
 }
